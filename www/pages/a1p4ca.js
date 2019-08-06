469f8527fe9724dac47375173d0b87998d23f4dc
김우재 <wj1187@naver.com>
2019-08-06 23:32:01 +0900

    a1p4ca useeffect second argument []

--- www/pages/a1p4ca.js
+++ www/pages/a1p4ca.js
@@ -168,7 +168,7 @@ function Admin({ postData, userData }) {
   useEffect(() => {
     const token = cookies.token
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-  })
+  }, [])
 
   return (
     <div>
